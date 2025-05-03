759f25dee1c08435855c629dedb97bfc593c332b
aphro-diet <zainasaffanah07@gmail.com>
2025-05-03 13:27:52 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,43 +1,60 @@
-function generateRecommendations() {
-    const mood = document.getElementById("mood").value;
-    const recommendationsDiv = document.getElementById("recommendations");
-    let recommendationsHTML = `<h2>Recommendations for ${mood.charAt(0).toUpperCase() + mood.slice(1)} Mood</h2>`;
-    
-    // Song recommendations for different moods
-    const songs = {
-        birthday: [
-            { name: "Birthday - Katy Perry", img: "https://upload.wikimedia.org/wikipedia/en/thumb/0/0a/Katy_Perry_Birthday.png/220px-Katy_Perry_Birthday.png", link: "https://www.youtube.com/watch?v=CEUg7OplvIQ" },
-            { name: "Happy Birthday - Stevie Wonder", img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cc/Stevie_Wonder_Happy_Birthday.jpg/220px-Stevie_Wonder_Happy_Birthday.jpg", link: "https://www.youtube.com/watch?v=F6FHehF5XyE" }
-        ],
-        sad: [
-            { name: "Someone Like You - Adele", img: "https://upload.wikimedia.org/wikipedia/en/thumb/d/d5/Adele_-_Someone_Like_You.png/220px-Adele_-_Someone_Like_You.png", link: "https://www.youtube.com/watch?v=hLQl3WQQoQ0" },
-            { name: "The Night We Met - Lord Huron", img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c0/Lord_Huron_-_The_Night_We_Met.png/220px-Lord_Huron_-_The_Night_We_Met.png", link: "https://www.youtube.com/watch?v=2xoGjpL9QQg" }
-        ],
-        happy: [
-            { name: "Happy - Pharrell Williams", img: "https://upload.wikimedia.org/wikipedia/en/thumb/2/26/Pharrell_Williams_-_Happy.png/220px-Pharrell_Williams_-_Happy.png", link: "https://www.youtube.com/watch?v=y6Sxv-sUYtM" },
-            { name: "Good as Hell - Lizzo", img: "https://upload.wikimedia.org/wikipedia/en/thumb/9/97/Lizzo_Good_as_Hell.png/220px-Lizzo_Good_as_Hell.png", link: "https://www.youtube.com/watch?v=8M1q1ST9_8g" }
-        ],
-        energetic: [
-            { name: "Uptown Funk - Mark Ronson ft. Bruno Mars", img: "https://upload.wikimedia.org/wikipedia/en/thumb/1/1b/Uptown_Funk%21_%28song%29.png/220px-Uptown_Funk%21_%28song%29.png", link: "https://www.youtube.com/watch?v=OPf0YbXqDm0" },
-            { name: "Can't Stop the Feeling! - Justin Timberlake", img: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Justin_Timberlake_-_Can%27t_Stop_the_Feeling%21.png/220px-Justin_Timberlake_-_Can%27t_Stop_the_Feeling%21.png", link: "https://www.youtube.com/watch?v=ru0K8uYEZWw" }
-        ],
-        romantic: [
-            { name: "Perfect - Ed Sheeran", img: "https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/Ed_Sheeran_-_Perfect.png/220px-Ed_Sheeran_-_Perfect.png", link: "https://www.youtube.com/watch?v=2Vv-BfVoq4g" },
-            { name: "All of Me - John Legend", img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/John_Legend_-_All_of_Me.png/220px-John_Legend_-_All_of_Me.png", link: "https://www.youtube.com/watch?v=450p7goxZqg" }
-        ]
-    };
+const vibes = {
+  birthday: [
+    { title: "Birthday - Katy Perry", cover: "https://upload.wikimedia.org/wikipedia/en/0/0a/Katy_Perry_Birthday.png", link: "https://www.youtube.com/watch?v=CEUg7OplvIQ" },
+    { title: "Happy Birthday - Stevie Wonder", cover: "https://upload.wikimedia.org/wikipedia/en/c/cc/Stevie_Wonder_Happy_Birthday.jpg", link: "https://www.youtube.com/watch?v=F6FHehF5XyE" }
+  ],
+  sad: [
+    { title: "Someone Like You - Adele", cover: "https://upload.wikimedia.org/wikipedia/en/d/d5/Adele_-_Someone_Like_You.png", link: "https://www.youtube.com/watch?v=hLQl3WQQoQ0" },
+    { title: "The Night We Met - Lord Huron", cover: "https://upload.wikimedia.org/wikipedia/en/c/c0/Lord_Huron_-_The_Night_We_Met.png", link: "https://www.youtube.com/watch?v=2xoGjpL9QQg" }
+  ],
+  happy: [
+    { title: "Happy - Pharrell Williams", cover: "https://upload.wikimedia.org/wikipedia/en/2/26/Pharrell_Williams_-_Happy.png", link: "https://www.youtube.com/watch?v=y6Sxv-sUYtM" },
+    { title: "Good as Hell - Lizzo", cover: "https://upload.wikimedia.org/wikipedia/en/9/97/Lizzo_Good_as_Hell.png", link: "https://www.youtube.com/watch?v=8M1q1ST9_8g" }
+  ],
+  energetic: [
+    { title: "Uptown Funk - Mark Ronson ft. Bruno Mars", cover: "https://upload.wikimedia.org/wikipedia/en/1/1b/Uptown_Funk%21_%28song%29.png", link: "https://www.youtube.com/watch?v=OPf0YbXqDm0" },
+    { title: "Can't Stop the Feeling! - Justin Timberlake", cover: "https://upload.wikimedia.org/wikipedia/en/a/a4/Justin_Timberlake_-_Can%27t_Stop_the_Feeling%21.png", link: "https://www.youtube.com/watch?v=ru0K8uYEZWw" }
+  ],
+  romantic: [
+    { title: "Perfect - Ed Sheeran", cover: "https://upload.wikimedia.org/wikipedia/en/2/2f/Ed_Sheeran_-_Perfect.png", link: "https://www.youtube.com/watch?v=2Vv-BfVoq4g" },
+    { title: "All of Me - John Legend", cover: "https://upload.wikimedia.org/wikipedia/en/c/c3/John_Legend_-_All_of_Me.png", link: "https://www.youtube.com/watch?v=450p7goxZqg" }
+  ],
+  relaxed: [
+    { title: "Sunflower - Post Malone & Swae Lee", cover: "https://upload.wikimedia.org/wikipedia/en/5/5a/Post_Malone_and_Swae_Lee_-_Sunflower.png", link: "https://www.youtube.com/watch?v=ApXoWvfEYVU" },
+    { title: "Weightless - Marconi Union", cover: "https://upload.wikimedia.org/wikipedia/commons/e/e2/Weightless_album_cover.jpg", link: "https://www.youtube.com/watch?v=UfcAVejslrU" }
+  ],
+  friends: [
+    { title: "Count on Me - Bruno Mars", cover: "https://upload.wikimedia.org/wikipedia/en/d/d1/Bruno_Mars_-_Doo-Wops_%26_Hooligans.png", link: "https://www.youtube.com/watch?v=yJYXItns2ik" },
+    { title: "Lean On Me - Bill Withers", cover: "https://upload.wikimedia.org/wikipedia/en/3/35/Bill_Withers_-_Lean_on_Me.jpg", link: "https://www.youtube.com/watch?v=fOZ-MySzAac" }
+  ],
+  aesthetic: [
+    { title: "Coffee - beabadoobee", cover: "https://i1.sndcdn.com/artworks-000242914675-zs1s4z-t500x500.jpg", link: "https://www.youtube.com/watch?v=J3uh3r3bDG4" },
+    { title: "Peach - Broods", cover: "https://upload.wikimedia.org/wikipedia/en/5/5c/Broods_-_Peach.png", link: "https://www.youtube.com/watch?v=ZxZyrK-h0hw" }
+  ],
+  adventure: [
+    { title: "Adventure of a Lifetime - Coldplay", cover: "https://upload.wikimedia.org/wikipedia/en/1/1f/Coldplay_-_Adventure_of_a_Lifetime.png", link: "https://www.youtube.com/watch?v=QtXby3twMmI" },
+    { title: "Road Trippin' - Red Hot Chili Peppers", cover: "https://upload.wikimedia.org/wikipedia/en/5/5d/Rhcp_road_trippin.jpg", link: "https://www.youtube.com/watch?v=PcAm2L368Ys" }
+  ],
+  chill: [
+    { title: "Sunset Lover - Petit Biscuit", cover: "https://upload.wikimedia.org/wikipedia/en/d/d3/Sunset_Lover_cover.jpg", link: "https://www.youtube.com/watch?v=FzlPXM8Ct7I" },
+    { title: "Ocean Eyes - Billie Eilish", cover: "https://upload.wikimedia.org/wikipedia/en/5/5b/Billie_Eilish_-_Ocean_Eyes.png", link: "https://www.youtube.com/watch?v=viimfQi_pUw" }
+  ]
+};
 
-    const selectedSongs = songs[mood] || [];
-    let songHTML = "";
-    selectedSongs.forEach(song => {
-        songHTML += `
-            <div class="song">
-                <h3>${song.name}</h3>
-                <img src="${song.img}" alt="${song.name}">
-                <a href="${song.link}" target="_blank">Listen on YouTube</a>
-            </div>
-        `;
-    });
+function showVibes(mood) {
+  const container = document.getElementById('vibeResults');
+  container.innerHTML = `<h2>${mood.charAt(0).toUpperCase()+mood.slice(1)} Vibes</h2>`;
+  vibes[mood].forEach(song => {
+    const card = `
+      <div class="vibe-card">
+        <img src="${song.cover}" alt="${song.title}" />
+        <p>${song.title}</p>
+        <a href="${song.link}" target="_blank">▶ Listen</a>
+      </div>`;
+    container.innerHTML += card;
+  });
+}
 
-    recommendationsDiv.innerHTML = recommendationsHTML + `<div class="song-container">${songHTML}</div>`;
+function showMoodBoard() {
+  document.getElementById('moodBoard').classList.toggle('hidden');
 }
